25feb3b25b3c4ee6a2f956f5c5ada08712050b0b
khassouma <80035724+khassouma@users.noreply.github.com>
2021-03-04 13:48:56 +0100

    Add files via upload

---
+++ Sportech/Inclusion/footer.php
@@ -0,0 +1,53 @@
+<footer class="container">
+    <div class="footer_ row"> 
+        <div class="footer_1 col-md-6">
+          <ul>
+              <li>
+                 <span><i class="fas fa-map-marker-alt"></i></span>
+                 <span> Banankabougou </span>
+              </li>
+              <br/>
+              <li>
+                 <span><i class="fas fa-phone-alt"></i></span>
+                 <span> 00000000 </span>
+              </li>
+              <br/>
+              <li>
+                 <span><i class="fas fa-envelope"></i></span>
+                 <span> tttt@ </span>
+              </li>
+          </ul>
+        </div>
+
+
+        <div class="footer_2 col-md-6">
+           <h3>Sportech</h3>
+           <p> En partenariat avec: </p>
+           <br/>
+           <div class="row">
+                <div class="sps_ col-md-12">
+                   <span><img  src="Images/logo/st.jpg"></span>
+                   <span><img  src="Images/logo/f.jpg"></span>
+                   <span><img  src="Images/logo/g.jpg"></span>
+               </div>
+      
+               <div class="sps_ col-md-12">
+                    <span><img  src="Images/logo/y.jpg"></span>
+                   <span><img  src="Images/logo/u.jpg"></span>
+                   <span><img  src="Images/logo/k.jpg"></span>
+                </div>
+
+               <div class="sps_ col-md-12">
+                   <span><img  src="Images/logo/ma.jpg"></span>
+                   <span><img  src="Images/logo/m.jpg"></span>
+                </div>
+           </div>
+           <br/>
+           <div>
+              <span><i class="fab fa-twitter-square"></i></span>
+              <span><i class="fab fa-facebook-square"></i></span>
+           </div>
+           
+        </div>       
+    </div>
+</footer>
